a3c86891b91490715ca9f77f89a0b7ebf18fc109
incrisz <incrisz4luv@gmail.com>
2025-07-01 01:31:55 +0100

    updated details

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = getenv('DB_HOST') ?: 'reconxi-ghcr-qtz2v7';
-$db   = getenv('DB_DATABASE') ?: 'testdb';
-$user = getenv('DB_USERNAME') ?: 'user';
-$pass = getenv('DB_PASSWORD') ?: 'password';
+$db   = getenv('DB_DATABASE') ?: 'ghcr';
+$user = getenv('DB_USERNAME') ?: 'ghcr';
+$pass = getenv('DB_PASSWORD') ?: 'ghcr';
 
 echo "Everything done on GHCR! 👋<br>";
 
